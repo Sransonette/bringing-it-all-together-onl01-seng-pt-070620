42e4272b794a369e22dcce87dd2125f7cc2f52d3
Sean Ransonette <sransonette@gmail.com>
2020-09-11 01:08:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,11 @@ class Dog
   attr_accessor :name, :breed
   
 
-  def initialize(name:, breed:, id = nil)
+  def initialize(id=nil, name:, breed:)
     
     @name = name
     @breed = breed
-  end
+    
   end
 
 end
