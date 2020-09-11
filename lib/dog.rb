547d9988915bfd07e4a1821d94dd61ce1e2774d4
Sean Ransonette <sransonette@gmail.com>
2020-09-11 01:51:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,9 +53,10 @@ class Dog
   def self.new_from_db(row)
     
     attributes = {
-    :id => row[0]
-    :name => row[1]
-    :breed => row[2]}
+    :id => row[0],
+    :name => row[1],
+    :breed => row[2]
+    }
     
     self.new(attributes)
     
