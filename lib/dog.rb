be3ef1303b67d371a665540d9e19ad4f36cb0b34
Sean Ransonette <sransonette@gmail.com>
2020-09-11 00:54:48 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+
+  attr_accessor :id, :name, :breed
+
+  def initialize(attributes)
+    #id: nil, name:, breed:
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
+end
+ 
