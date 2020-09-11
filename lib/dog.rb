2b5a1ac2afd2ca191d01c5193a1b2d302e297cdd
Sean Ransonette <sransonette@gmail.com>
2020-09-11 01:53:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,7 @@ class Dog
   
   def self.find_by_id(id)
     sql = "SELECT *
-           FROM students
+           FROM dogs
            WHERE name = ?"
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
