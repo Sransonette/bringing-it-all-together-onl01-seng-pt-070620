709872d7b8058a654b059a9d517114152f0992ad
Sean Ransonette <sransonette@gmail.com>
2020-09-11 01:34:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,8 @@ class Dog
 
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-  binding.pry
+  #binding.pry
+    Dog
   end
   
   
