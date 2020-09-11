4e35eec6949ec287a07e259e918c7cdba9bb3da1
Sean Ransonette <sransonette@gmail.com>
2020-09-11 01:22:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,7 +44,11 @@ class Dog
    end
   end
   
-  
+  def update
+    sql = "UPDATE students SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+
+  end
   
 
 end
